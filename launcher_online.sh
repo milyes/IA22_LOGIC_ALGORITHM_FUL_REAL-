29b8d5f95bfde44924771cffb6762f3671a7b8e9
Mohammed Ilyes Zoubirou <ton_email@gmail.com>
2026-06-20 18:17:17 -0400

    Initial commit – version principale IA logique

--- launcher_online.sh
+++ launcher_online.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 URL="https://milyes.github.io/IA22_LOGIC_ALGORITHM_FUL_REAL/"
 
-echo "🌐 Lancement en ligne de IA22_LOGIC_ALGORITHM_FUL_REAL..."
+echo "🌐 Lancement en ligne de IA22_LOGIC_ALGORITHM_FUL_REAL/"
 # Ouvre l'URL dans le navigateur par défaut
 if command -v xdg-open &> /dev/null; then
     xdg-open "$URL"
